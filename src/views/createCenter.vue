739ca7de4a06e425e885b60e867b4a0490775959
chenyuliang-123 <chenyuliang2022lai@163.com>
2024-11-28 20:43:02 +0800

    111

---
+++ src/views/createCenter.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="common-layout">
+      <el-container>
+        <el-header class="header">
+            <img  class="idea" src="../assets/logo.png" alt="idea">
+            <span class="title">创作中心</span>
+        </el-header>
+        <el-container>
+          <el-aside class="side" width="200px">
+            <nav>
+               <ul>
+                <li><router-link to="/createCenter/notePost">发布笔记</router-link></li>
+                <li><router-link to="/createCenter/noteManager">管理笔记</router-link></li>
+                <li><a href="#/ai" target="'_blank'">AI助手</a></li>
+               </ul>
+            </nav>
+          </el-aside>
+          <el-main>
+            <router-view></router-view>
+      <!-- 渲染子路由 -->
+          </el-main>
+        </el-container>
+      </el-container>
+    </div>
+</template>
+<script>
+</script>
+<style scoped>
+    .header{
+        background-color: #2c3e50;
+        height: 80px !important;   
+        display: flex;
+        justify-content: center;
+        align-items: center; 
+       
+    }
+    .idea{
+        width: 50px;
+        margin-right: 10px;      
+    }
+    .title{
+       
+        color:white;
+        font-size: 25px;
+
+    }
+    .side{
+        height: 100%;
+        color: white;
+    }
+    .size nav {
+        display: flex;
+        justify-content: center; /* 左右分布 */
+        align-items: center; /* 垂直居中 */
+        list-style: none;
+    }
+    .side li{
+        margin-top:30px;
+    }
+    .side li a{
+        text-decoration: none;
+        margin-top:30px;
+        font-size:20px;
+        color:#2c3e50;
+        padding: 10px;
+    }
+    .side li a:hover{
+        color: red;
+    }
+    .router-link-active {
+        background-color: #42b983; /* 选中的项变色 */
+        color: white; /* 选中的项文字颜色 */
+        border-radius: 5px;
+        }
+</style>
---
+++ src/views/notePost.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container">
+        <div class="title">
+            <button class="button-img" @click="uploadImg" >上传图片</button>
+            <button class="button-video" @click="uploadVideo">上传视频</button>
+        </div>
+        <div class="upload-container">
+            <div 
+                class="upload-box" 
+                @drop.prevent="handleDrop" 
+                @dragover.prevent
+            >
+                <p>拖拽文件到这里上传</p>
+                <input 
+                    type="file" 
+                    @change="handleFileUpload" 
+                    ref="fileInput" 
+                    style="display: none;" 
+                />
+                <button @click="triggerFileInput">选择文件</button>
+            </div>
+        </div>
+   </div>
+</template>
+<script lang="ts" setup>
+    import {ref} from 'vue'
+    let isImgButtonActive=ref(false)
+    let isVideoButtonActive=ref(false)
+    function uploadImg(){
+        isImgButtonActive.value=true;
+    }
+</script>
+<style scoped>
+    .container{
+        
+        background-color: #d9ecff;
+    }
+    .title{
+        height:30px;
+        background-color: white;
+        font-size:15px;
+        margin-left:0px;
+        align-items: center;
+        margin-top:5px;
+    }
+    .button-img,.button-video{
+        border:none;
+        background-color: #d3d3d3;
+        font-size: 20px;
+        cursor: pointer;
+        
+    }
+    .button-img.active,
+    .button-video.active {
+        background-color: black;
+        color: white; /* 点击后变黑 */
+    }
+    .upload-container{
+        margin-top:0px;
+    }
+    
+</style>
